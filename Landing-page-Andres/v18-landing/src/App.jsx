7690e183be56b5dfdea4ed54411ba0326f596a04
Admin Console Dev <aesquive@local.dev>
2026-04-29 23:22:40 -0400

    feat: assemble full editorial landing page

--- Landing-page-Andres/v18-landing/src/App.jsx
+++ Landing-page-Andres/v18-landing/src/App.jsx
@@ -1,43 +1,24 @@
-import { useEffect } from 'react'
-import Lenis from 'lenis'
-import gsap from 'gsap'
-import { ScrollTrigger } from 'gsap/ScrollTrigger'
-import Entrance from './components/Entrance'
-import About from './components/About'
-import Services from './components/Services'
-import Proof from './components/Proof'
-import Process from './components/Process'
-import CTA from './components/CTA'
-
-gsap.registerPlugin(ScrollTrigger)
+import SmoothScroll from './components/SmoothScroll';
+import Hero from './components/Hero';
+import Tension from './components/Tension';
+import Services from './components/Services';
+import Philosophy from './components/Philosophy';
+import Testimonials from './components/Testimonials';
+import CTA from './components/CTA';
+import Footer from './components/Footer';
 
 export default function App() {
-  useEffect(() => {
-    const lenis = new Lenis({
-      duration: 1.4,
-      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-      smoothWheel: true,
-    })
-
-    lenis.on('scroll', ScrollTrigger.update)
-
-    gsap.ticker.add((time) => {
-      lenis.raf(time * 1000)
-    })
-
-    gsap.ticker.lagSmoothing(0)
-
-    return () => { lenis.destroy() }
-  }, [])
-
   return (
-    <main>
-      <Entrance />
-      <About />
-      <Services />
-      <Proof />
-      <Process />
-      <CTA />
-    </main>
-  )
+    <SmoothScroll>
+      <main>
+        <Hero />
+        <Tension />
+        <Services />
+        <Philosophy />
+        <Testimonials />
+        <CTA />
+        <Footer />
+      </main>
+    </SmoothScroll>
+  );
 }
